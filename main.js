d75c08d3781de031dbd414b72554416dfb12e386
takuyakasagawa <kasa.kasa.0062362@gmail.com>
2019-09-05 02:16:52 +0900

    edited JS

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-
+// 1つ目の関数:キーボードが押されたら音が鳴って該当のキーボードに赤枠がつく
+function playingAdd(e) { 
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    console.log(key);
+    //クラス属性playingの記述
+    
+  }
+playingAdd(); // 呼び出し（使う）
+//関数playingAddをイベントを呼び出す記述の中で呼び出す
+// 2つ目の関数:キーボードが離れたら音が消えて赤枠が消える

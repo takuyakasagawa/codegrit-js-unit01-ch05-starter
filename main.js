9f48160c1b21a5a80301791df15f6d5a750e07e8
takuyakasagawa <kasa.kasa.0062362@gmail.com>
2019-09-05 02:51:18 +0900

    edited JS

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ function playingAdd(e) {
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     console.log(key);
     //クラス属性playingの記述
+    key.addEventListener('keydown',function(){
+      )
     
   }
 playingAdd(); // 呼び出し（使う）
